ba966ddb4c2d84d65b88df64816cf54740a8e87a
nextmetaphor <paul@nextmetaphor.com>
2022-02-03 23:29:31 +0000

    addition of simple json fetch code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {TaxonomyService} from "./taxonomy.service";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'cloud-blueprints-ui';
+    title = 'cloud-blueprints-ui';
+
+    taxonomy: any[] = [];
+
+    constructor(private taxonomyService: TaxonomyService) {
+    }
+
+    ngOnInit() {
+        this.taxonomyService.getTaxonomy().subscribe(sr => {
+            Object.assign(this.taxonomy, sr);
+        });
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,27 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatSliderModule } from '@angular/material/slider';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatSliderModule} from '@angular/material/slider';
+
+import {HttpClientModule} from '@angular/common/http';
+import {TaxonomyService} from "./taxonomy.service";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    MatSliderModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        MatSliderModule,
+        HttpClientModule,
+    ],
+    providers: [TaxonomyService],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
